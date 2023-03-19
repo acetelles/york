0b96d46cc83713d60891bced1a24a68c0409e5bc
acetelles <115594718+acetelles@users.noreply.github.com>
2023-03-19 16:10:42 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-j1pcWVe4JXzYNc80HVb4VHpR1ZdWolU",
+    authDomain: "prueba-clase-9138b.firebaseapp.com",
+    projectId: "prueba-clase-9138b",
+    storageBucket: "prueba-clase-9138b.appspot.com",
+    messagingSenderId: "1015496112861",
+    appId: "1:1015496112861:web:22dea809fe5cb9f567f934"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
