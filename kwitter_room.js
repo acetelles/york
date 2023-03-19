159e6dc231afb1f21161268f698d129a20a1d4c6
acetelles <115594718+acetelles@users.noreply.github.com>
2023-03-19 17:13:11 -0600

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -9,3 +9,27 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+
+
+
+  user_name = localStorage.getItem("user_item");
+  room_name = localStorage.getItem("room_name");  
+
+  document.getElementById("userName").innerHTML = "¡Bienvenido " + user_name + "!"
+
+
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+    Room_names = childKey;
+    //Inicia código, help xdd
+    
+    row = "<div class='room_name' id="> +Room_names+ " onclick ='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+
+    //Termina código
+    });});}
+    getData();
+
+    function redirectToRoomName {
+        document.getElementById("roomName").value = room_name;
+        window.location = "index.html"
+    }
+
